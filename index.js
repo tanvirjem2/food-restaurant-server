ec4c80789df8ce87bc28e6caeb73b7cdda5bcb73
tanvirjem2 <tanviranjum046@gmail.com>
2023-11-30 13:07:14 +0600

    now we can get only one data from database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -36,6 +36,15 @@ async function run() {
         app.get('/foods', async (req, res) => {
             const cursor = foodCollection.find();
             const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        // ----------- Get "Only one data" from foods -------------
+
+        app.get('/foods/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await foodCollection.findOne(query);
             res.send(result)
         })
 
